45205125b9f14e0ef736cfcb822d6fed3e9782c6
Noel-96 <noelorion69@gmail.com>
2019-09-20 14:02:48 +0100

    Initial Commit

---
+++ BreakPoint/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BreakPoint
+//
+//  Created by Noel Obaseki on 20/09/2019.
+//  Copyright © 2019 Tizeti. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
